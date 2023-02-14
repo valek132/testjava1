a827744121602431f298f5a09ea9bd7a36554ad9
Valek <pasport96@gmail.com>
2023-02-15 00:10:37 +0300

    first commit

---
+++ Task1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите первое число: ");
+        int i = iScanner.nextInt();
+        System.out.printf("Треугольное число: %d\n", giveMeNumber(i));
+        iScanner.close();
+    }
+
+    public static int giveMeNumber(int a) {
+        return (a * (a + 1)) / 2;
+    }
+}
